56be784f2256062a014d20b2d78de77ec0004ef0
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-11-11 20:58:52 +0300

    use onSubmit test

--- src/Form.test.jsx
+++ src/Form.test.jsx
@@ -1,10 +1,24 @@
-import { describe, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import {StateForm as Form} from "./StateForm"
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("Form component", () => {
-    it("should call onSubmit when the form is validwith the correct data", () => {
+    it("should call onSubmit when the form is validwith the correct data", async () => {
         const onSubmit = vi.fn()
+        const user = userEvent.setup()
         render(<Form onSubmit={onSubmit}/>)
+        const email = "test@webdevsimplified.com"
+        const password = "Password123"
+
+        await user.type(screen.getByLabelText("Email"), email)
+        await user.type(screen.getByLabelText("Password"), password)
+        await user.click(screen.getByText("Submit"))
+
+        expect(screen.queryByTestId("email-error-msg")).not.toBeInTheDocument()
+        expect(screen.queryByTestId("password-error-msg")).not.toBeInTheDocument()
+
+        expect(onSubmit).toHaveBeenCalledOnce()
+        expect(onSubmit).toHaveBeenCalledWith({email, password})
     })
 })

adcad1199223d52212c4b17a7fcc78ae30375a8f
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-11-11 21:08:41 +0300

    testing error messages on submit

--- src/Form.test.jsx
+++ src/Form.test.jsx
@@ -21,4 +21,36 @@ describe("Form component", () => {
         expect(onSubmit).toHaveBeenCalledOnce()
         expect(onSubmit).toHaveBeenCalledWith({email, password})
     })
+
+    it("should show the error message when the email is invalid", async() => {
+        const onSubmit = vi.fn()
+        const user = userEvent.setup()
+        render(<Form onSubmit={onSubmit}/>)
+        const email = "test@test.com"
+        const password = "Password123"
+        
+        await user.type(screen.getByLabelText("Email"), email)
+        await user.type(screen.getByLabelText("Password"), password)
+        await user.click(screen.getByText("Submit"))
+
+        expect(screen.getByTestId("email-error-msg")).toBeInTheDocument()
+
+        expect(onSubmit).not.toHaveBeenCalledOnce()
+    })
+
+    it("should show the error message when the password is invalid", async() => {
+        const onSubmit = vi.fn()
+        const user = userEvent.setup()
+        render(<Form onSubmit={onSubmit}/>)
+        const email = "test@webdevsimplified.com"
+        const password = "123"
+        
+        await user.type(screen.getByLabelText("Email"), email)
+        await user.type(screen.getByLabelText("Password"), password)
+        await user.click(screen.getByText("Submit"))
+
+        expect(screen.getByTestId("password-error-msg")).toBeInTheDocument()
+
+        expect(onSubmit).not.toHaveBeenCalledOnce()
+    })
 })
